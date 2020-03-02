613674a08113ebef8e091dde4fd7e5afcdbb2c43
BAKER DW <BAKER DW@172.16.228.172>
2020-03-02 12:41:03 +0200

    the first upload without testing

---
+++ src/calcolater/clacolater.java
@@ -0,0 +1,39 @@
+package calcolater;
+
+public class clacolater {
+	public static void main ( String args []) { 
+		int x= 5 ; 
+		int y=5 ; 
+		
+		System.out.println( "add :  "+add(x,y )); 
+		System.out.println( "- :  "+sub(x,y )); 
+		System.out.println( "* :  "+multi(x,y )); 
+		System.out.println( "/ :  "+div(x,y )); 
+		
+		
+	}
+
+	public static double add ( double x, double y ) { 
+		return x+y  ; 
+		
+	}
+	
+	
+	public static double sub ( double x, double y ) { 
+		return x-y  ; 
+		
+	}
+	
+	public static double multi ( double x, double y ) { 
+		return x*y  ; 
+		
+	}
+	public static double div  ( double x, double y ) { 
+		return x/y  ; 
+		
+	}
+	
+	
+	 
+	
+}
---
+++ src/calcolater/clacolaterTest.java
@@ -0,0 +1,34 @@
+package calcolater;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class clacolaterTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMulti() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiv() {
+		fail("Not yet implemented");
+	}
+
+}
